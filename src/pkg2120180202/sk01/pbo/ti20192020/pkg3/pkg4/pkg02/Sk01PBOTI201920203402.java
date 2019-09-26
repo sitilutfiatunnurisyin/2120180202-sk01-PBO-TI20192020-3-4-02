90bca9a9f0d2448f9a73ab3b4f2b9440d66ae416
sitilutfiatunnurisyin <55843840+sitilutfiatunnurisyin@users.noreply.github.com>
2019-09-26 21:44:22 +0700

    APLOUD PROJECK 2

---
+++ src/pkg2120180202/sk01/pbo/ti20192020/pkg3/pkg4/pkg02/Sk01PBOTI201920203402.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2120180202.sk01.pbo.ti20192020.pkg3.pkg4.pkg02;
+
+/**
+ *
+ * @author asus
+ */
+public class Sk01PBOTI201920203402 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int a=1;a<=9;a++){
+            for(int b=9;b>=a;b--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=a;c++){
+                System.out.print("2");
+            }
+            System.out.println();
+        }
+    }
+    
+}
